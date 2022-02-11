ef9f8ef23cff14e34f8b745d2bf4e6d700098753
julianoleo <50123832+julianoleo@users.noreply.github.com>
2022-02-11 19:26:15 -0300

    Ajustes 5

--- src/main/java/com/juliano/apichaves/controller/ChavesController.java
+++ src/main/java/com/juliano/apichaves/controller/ChavesController.java
@@ -27,19 +27,19 @@ public class ChavesController {
     @Autowired
     private Autentication autentication;
 
-//    @GetMapping("/{idChave}")
-//    public ResponseEntity<Chaves> cadastraChave(
-//            HttpServletRequest request,
-//            @PathVariable(name = "idChave") String idChave,
-//            @RequestHeader HttpHeaders headers
-//    ) {
-//            autentication.verificaAuth(request, headers);
-//            var _result = chavesService.buscaPorId(idChave);
-//            var _response = new ResponseEntity<>(_result, HttpStatus.OK);
-//            var _responseLog = new ResponseDto<>(_result);
-//            APILogger.ok(_responseLog.getData(), APILogger.filterHeader(headers));
-//            return _response;
-//    }
+    @GetMapping("/{idChave}")
+    public ResponseEntity<Chaves> cadastraChave(
+            HttpServletRequest request,
+            @PathVariable(name = "idChave") String idChave,
+            @RequestHeader HttpHeaders headers
+    ) {
+            autentication.verificaAuth(request, headers);
+            var _result = chavesService.buscaPorId(idChave);
+            var _response = new ResponseEntity<>(_result, HttpStatus.OK);
+            var _responseLog = new ResponseDto<>(_result);
+            APILogger.ok(_responseLog.getData(), APILogger.filterHeader(headers));
+            return _response;
+    }
 
     @PostMapping("/add")
     public ResponseEntity<Chaves> cadastraChave(
